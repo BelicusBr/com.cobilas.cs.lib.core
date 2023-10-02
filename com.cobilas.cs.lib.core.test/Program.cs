8a6b1ee7d88dc556e09a2b05ac838ae5a62b1002
BelicusBr <x70r@outlook.com>
2023-10-02 14:50:38 -0300

    #Update

---
+++ com.cobilas.cs.lib.core.test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.cobilas.cs.lib.core.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
